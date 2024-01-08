0f2fa697039a44f6059116cf26652b1daaa1d452
Honcharenko Danylo Dmytrovych <danilo.gonch@gmail.com>
2024-01-08 12:27:08 +0200

    test: created controller and test for him

---
+++ analytics-service/src/main/java/com/jobflowengine/analytics/controller/AnalyticsServiceController.java
@@ -0,0 +1,15 @@
+package com.jobflowengine.analytics.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AnalyticsServiceController {
+    @GetMapping("/hello-world")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello world!");
+    }
+}
---
+++ analytics-service/src/test/java/com/jobflowengine/analytics/controller/AnalyticsServiceControllerTest.java
@@ -0,0 +1,22 @@
+package com.jobflowengine.analytics.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AnalyticsServiceController.class)
+public class AnalyticsServiceControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void getHelloWorld() throws Exception {
+        mockMvc.perform(get("/api/hello-world"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
