9b8f9a2f36577882550d163910a5382d94a0cca0
Honcharenko Danylo Dmytrovych <danilo.gonch@gmail.com>
2024-01-08 12:05:37 +0200

    setup: CI/CD setup in main.yaml file, renamed test class in analytics-service, added maven files mvnw, mvnw.cmd

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+# JobFlowEngine
 ![JobSchedulerService](high-level-scheme.svg)
---
+++ analytics-service/src/test/java/com/jobflowengine/analytics/AnalyticsServiceTest.java
@@ -0,0 +1,11 @@
+package com.jobflowengine.analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AnalyticsServiceTest {
+    @Test
+    public void contextLoad() {
+    }
+}
